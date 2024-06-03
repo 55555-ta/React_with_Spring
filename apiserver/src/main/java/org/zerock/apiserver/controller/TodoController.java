bd6c8422f239ceaa5bcfd29ebe92251c4668e25d
(55555ta) <stiker3615@gmail.com>
2024-06-03 23:35:15 +0900

    Basic API setup complete

--- apiserver/src/main/java/org/zerock/apiserver/controller/TodoController.java
+++ apiserver/src/main/java/org/zerock/apiserver/controller/TodoController.java
@@ -3,14 +3,13 @@ package org.zerock.apiserver.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.zerock.apiserver.dto.PageRequestDTO;
 import org.zerock.apiserver.dto.PageResponseDTO;
 import org.zerock.apiserver.dto.TodoDTO;
 import org.zerock.apiserver.service.TodoService;
+
+import java.util.Map;
 
 @RestController
 @Log4j2
@@ -34,4 +33,38 @@ public class TodoController {
 
         return todoService.getList(pageRequestDTO);
     }
+
+    @PostMapping("/")
+    public Map<String, Long> register(@RequestBody TodoDTO dto){
+
+        log.info("todoDTO" + dto);
+
+        Long tno = todoService.register(dto);
+
+        return Map.of("TNO", tno);
+
+    }
+
+    @PutMapping("/{tno}")
+    public Map<String, String> modify(@PathVariable("tno") Long tno,
+                                      @RequestBody TodoDTO todoDTO){
+
+        todoDTO.setTno(tno);
+
+        todoService.modify(todoDTO);
+
+
+        return Map.of("RESULT","SUCCESS");
+    }
+
+    @DeleteMapping("{tno}")
+    public Map<String, String> remove(@PathVariable Long tno){
+
+        todoService.remove(tno);
+
+        return Map.of("RESULT","SUCCESS");
+
+
+    }
+
 }
